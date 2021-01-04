a5402d26a732c1af5492759edee2d2d8ca181877
Azat Idrisov <azzzat72@yandex.ru>
2021-01-04 09:48:14 +0500

    2.1. Преобразование даты [#348679]

---
+++ src/main/java/ru/job4j/html/FormateDate.java
@@ -0,0 +1,29 @@
+package ru.job4j.html;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class FormateDate {
+
+    private static final String PATTERN = "dd MMM yy, HH:mm";
+
+    public static Date formate(String date) throws ParseException {
+        Date today = new Date();
+        Calendar day = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMM yy,");
+        String[] str = date.split(" ");
+        if (str[0].contains("сегодня")) {
+            str[0] = dateFormat.format(today);
+        } else if (str[0].contains("вчера")) {
+            day.add(Calendar.DAY_OF_YEAR, -1);
+            str[0] = dateFormat.format(day.getTime());
+        } else if (str[1].equals("сен")) {
+            str[1] = "сент.";
+        } else {
+            str[1] = str[1] + ".";
+        }
+        return new SimpleDateFormat(PATTERN).parse(String.join(" ", str));
+    }
+}
--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -5,11 +5,6 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-
 public class SqlRuParse {
 
     public static void main(String[] args) throws Exception {
@@ -19,26 +14,7 @@ public class SqlRuParse {
             Element href = td.child(0);
             System.out.println(href.attr("href"));
             Element date = td.parent().child(5);
-            System.out.println(href.text() + " " + SqlRuParse.formateDate(date.text()));
+            System.out.println(href.text() + " " + FormateDate.formate(date.text()));
         }
-    }
-
-    public static String formateDate(String date) throws ParseException {
-        Date today = new Date();
-        Calendar day = Calendar.getInstance();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMM yy,");
-        String[] str = date.split(" ");
-        if (str[0].contains("сегодня")) {
-            str[0] = dateFormat.format(today);
-        } else if (str[0].contains("вчера")) {
-            day.add(Calendar.DAY_OF_YEAR, -1);
-            str[0] = dateFormat.format(day.getTime());
-        } else if (str[1].equals("сен")) {
-            str[1] = "сент.";
-        } else {
-            str[1] = str[1] + ".";
-        }
-        Date newDate = new SimpleDateFormat("dd MMM yy, HH:mm").parse(String.join(" ", str));
-        return new SimpleDateFormat("dd-MM-yyyy HH:mm").format(newDate);
     }
 }
