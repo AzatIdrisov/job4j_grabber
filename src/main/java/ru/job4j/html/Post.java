6f690e1747f72c3bc63e2a3fe62dc23ab5e38de3
Azat Idrisov <azzzat72@yandex.ru>
2021-01-06 16:32:25 +0500

    2.2. Модель данных - Post. [#348676]

--- src/main/java/ru/job4j/html/Post.java
+++ src/main/java/ru/job4j/html/Post.java
@@ -4,15 +4,17 @@ import java.util.Date;
 
 public class Post {
     private String postName;
+    private String link;
     private String author;
     private Date dateCreation;
     private int id;
     private String text;
 
-    public Post(String postName, String text, String author, Date date) {
+    public Post(String postName, String text, String author, Date date, String link) {
         this.postName = postName;
         this.text = text;
         this.author = author;
         this.dateCreation = date;
+        this.link = link;
     }
 }
